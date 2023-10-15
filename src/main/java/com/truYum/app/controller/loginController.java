c07a657c7b691e147e0802465baa296a147bd965
CoderAvijit <95471951+CoderAvijit@users.noreply.github.com>
2023-10-15 21:36:14 +0530

    First Commit

--- src/main/java/com/truYum/app/controller/loginController.java
+++ src/main/java/com/truYum/app/controller/loginController.java
@@ -1,5 +1,7 @@
 package com.truYum.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,33 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.truYum.app.Repository.*;
+import com.truYum.app.response.*;
+import com.truYum.app.persitant.*;
 @RestController
 @RequestMapping("/login")
 @ResponseBody
 public class loginController {
 	@Autowired
 	private loginRepo repo;
-	@CrossOrigin(origins = "http://localhost:3000")
+	@CrossOrigin(origins = "*")
 	@PostMapping("/logincontroller")
-	public ModelAndView doLogin(@RequestParam String role,@RequestParam String email,
+	public ResponseEntity<Object> doLogin(@RequestParam String role,@RequestParam String email,
 	        @RequestParam String password) {
 		String redirectUrl = repo.doLogin(role,email, password);
 		 if (redirectUrl.equals("/addmenu.html")) {
 	            // If it's an admin login, redirect to the admin menu HTML page in the static directory
-			 ModelAndView modelAndView = new ModelAndView();
-			    modelAndView.setViewName("addmenu.html");
-			    System.out.println(modelAndView);
-			    return modelAndView;
+			return ResponseHandler.generateResponse("Succesfull",HttpStatus.OK);
 	        } else if(redirectUrl.equalsIgnoreCase("adminpage")) {
-	        	ModelAndView modelAndView = new ModelAndView();
-			    modelAndView.setViewName("editmenu");
-			    return modelAndView;
+	        	return new ResponseEntity<Object>("Role :"+role+"Id: "+email,HttpStatus.PRECONDITION_FAILED);
 	        }
 	        	else {
 	            // Handle other redirects or error messages as needed
-	        	ModelAndView modelAndView = new ModelAndView();
-			    modelAndView.setViewName("ErrorPage");
-			    return modelAndView;
+	        		return new ResponseEntity<Object>(HttpStatus.INTERNAL_SERVER_ERROR);
 	        }
 	}
 }
